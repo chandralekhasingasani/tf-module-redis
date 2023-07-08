d2867f84b20fa43b219c8b23f998c0441b689995
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-07-08 19:15:32 +0530

    test

--- pg.tf
+++ pg.tf
@@ -1,4 +1,8 @@
 resource "aws_elasticache_parameter_group" "main" {
   name   = "${var.COMPONENT}-${var.ENV}"
   family = var.FAMILY
+  parameter {
+    name = "ClusterEnabled"
+    value = "true"
+  }
 }
