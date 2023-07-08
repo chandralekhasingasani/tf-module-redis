08d35d9947d591e49498ea294747769d6efaceea
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-07-08 19:28:30 +0530

    test

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_elasticache_cluster" "main" {
   engine               = var.ENGINE
   node_type            = var.NODE_TYPE
   num_cache_nodes      = var.NUM_CACHE_NODES
-  parameter_group_name = aws_elasticache_parameter_group.main.name
+  parameter_group_name = "default.redis6.x.cluster.on"
   engine_version       = var.ENGINE_VERSION
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.main.name
